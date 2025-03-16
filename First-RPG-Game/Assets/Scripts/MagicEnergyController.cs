7ad8db7f510b899d2db2a3ef0ae4d3af70615101
NA <nhatanhtruong20@gmail.com>
2025-03-16 14:31:12 +0700

    assets commit

---
+++ First-RPG-Game/Assets/Scripts/MagicEnergyController.cs
@@ -0,0 +1,38 @@
+using Stats;
+using UnityEngine;
+
+public class MagicEnergyController : MonoBehaviour
+{
+    [SerializeField] private string targetLayerName = "Player";
+    [SerializeField] private int damage;
+
+    [SerializeField] private float xVelocity;
+    [SerializeField] private Rigidbody2D rb;
+
+    [SerializeField] private bool canMove = true;
+    [SerializeField] private bool flipped;
+    
+    private CharacterStats _myStats;
+
+    public void SetupMagicEnergy(float speed, CharacterStats stats)
+    {
+        xVelocity = speed;
+        _myStats = stats;
+    }
+    
+    private void Update()
+    {
+        if (canMove)
+            rb.linearVelocity = new Vector2(xVelocity, rb.linearVelocity.y);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.layer == LayerMask.NameToLayer(targetLayerName))
+        {
+            _myStats.DoDamage(collision.GetComponent<CharacterStats>());
+            Destroy(gameObject);
+        }
+
+    }
+}
