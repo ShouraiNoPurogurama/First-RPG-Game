d3897b2ed89f40d15aac0f130d75729727556709
nctan123 <canhtancanhtan@gmail.com>
2025-03-20 03:10:48 +0700

    feat: add counterAtk

--- First-RPG-Game/Assets/Scripts/Enemies/Enemies_Fire/MeteoriteSkillFall.cs
+++ First-RPG-Game/Assets/Scripts/Enemies/Enemies_Fire/MeteoriteSkillFall.cs
@@ -9,34 +9,51 @@ namespace Enemies.Enemies_Fire
         public Transform groundCheck;
         public Transform wallCheck;  
         public LayerMask whatIsGround; 
-        public float checkDistance = 0.2f; 
+        public float checkDistance = 0.2f;
+        private Animator animator;
+        private bool isExploding = false;
+        //private MeteoriteAnimationEvents meteoriteEvents;
         // Start is called once before the first execution of Update after the MonoBehaviour is created
         void Start()
         {
-        
+            //Animator animator = meteorite.GetComponent<Animator>();
+            animator = GetComponent<Animator>();
+            //meteoriteEvents = GetComponent<MeteoriteAnimationEvents>();
         }
 
         // Update is called once per frame
         void Update()
         {
-        
+            if (isExploding) return;
+
             transform.position += Vector3.down * fallSpeed * Time.deltaTime;
+            Debug.Log(IsGroundDetected() + "............" + IsWallDetected());
             if (IsGroundDetected() || IsWallDetected())
             {
-                Destroy(gameObject);
+                TriggerAnimation("Explode");
+                //Destroy(gameObject);
             }
         }
 
         private void OnTriggerEnter2D(Collider2D collision)
         {
+            if (isExploding) return;
             //Debug.Log("Cham roi");
             Player player = collision.GetComponent<Player>();
             //Debug.Log(player);
             if (player != null)
             {
+                //Debug.Log(player.StateMachine.CurrentState);
+                if (player.StateMachine.CurrentState is PlayerCounterAttackState)
+                {
+                    TriggerAnimation("Explode");
+                    //Destroy(gameObject);
+                    return;
+                }
                 //Debug.Log("Cham roi ne");
                 player.Stats.TakeDamage(30, Color.yellow);
-                Destroy(gameObject);
+                TriggerAnimation("Explode");
+                //Destroy(gameObject);
             }
         
 
@@ -54,6 +71,11 @@ namespace Enemies.Enemies_Fire
         private bool IsWallDetected()
         {
             return Physics2D.Raycast(wallCheck.position, Vector2.right, checkDistance, whatIsGround);
+        }
+        private void TriggerAnimation(string animName)
+        {
+            isExploding = true;
+            animator.SetTrigger(animName);
         }
     }
 }
