b2eff56856f1e36a484bea659a81102bde40f96a
Datdater <datnqse173081@fpt.edu.vn>
2025-03-28 19:33:19 +0700

    Update general

---
+++ First-RPG-Game/Assets/Scripts/UI/HPPlayerBar_UI.cs
@@ -0,0 +1,64 @@
+﻿using Stats;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace UI
+{
+    public class HPPlayerBar_UI : MonoBehaviour
+    {
+        private Entity _entity;
+        private CharacterStats _characterStats;
+        private RectTransform _rectTransform;
+        private Slider _slider;
+        private Image _fillImage; 
+
+        [SerializeField] private Color normalColor = Color.red;
+        [SerializeField] private Color lowHPColor = Color.yellow; 
+
+        private void Start()
+        {
+            _rectTransform = GetComponent<RectTransform>();
+            _entity = GetComponentInParent<Entity>();
+            _slider = GetComponentInChildren<Slider>();
+            _characterStats = GameObject.Find("Player"). GetComponent<CharacterStats>();
+
+            // Lấy component Image từ Fill Area
+            _fillImage = _slider.fillRect.GetComponent<Image>();
+
+            _characterStats.OnHPChanged += UpdateHealthUI;
+            _entity.OnFlipped += FlipUI;
+
+            UpdateHealthUI();
+        }
+
+        private void UpdateHealthUI()
+        {
+            float maxHp = _characterStats.GetMaxHealthValue();
+            float currentHp = _characterStats.currentHp;
+
+            _slider.maxValue = maxHp;
+            _slider.value = currentHp;
+
+            // change if hp < 30%
+            if (currentHp / maxHp <= 0.3f)
+            {
+                _fillImage.color = lowHPColor;
+            }
+            else
+            {
+                _fillImage.color = normalColor;
+            }
+        }
+
+        private void FlipUI()
+        {
+            _rectTransform.Rotate(0, 180, 0);
+        }
+
+        private void OnDisable()
+        {
+            _entity.OnFlipped -= FlipUI;
+            _characterStats.OnHPChanged -= UpdateHealthUI;
+        }
+    }
+}
