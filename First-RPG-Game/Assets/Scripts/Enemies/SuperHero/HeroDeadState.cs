b8a7a2b3a324f32a4023b658639bf7d1fec2d274
nctan123 <canhtancanhtan@gmail.com>
2025-03-29 13:31:36 +0700

    pull

---
+++ First-RPG-Game/Assets/Scripts/Enemies/SuperHero/HeroDeadState.cs
@@ -0,0 +1,43 @@
+﻿using Enemies;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+public class HeroDeadState : EnemyState
+{
+    private Hero _Hero;
+    public HeroDeadState(Enemy enemyBase, EnemyStateMachine stateMachine, string animBoolName, Hero Hero) : base(enemyBase, stateMachine, animBoolName)
+    {
+        _Hero = Hero;
+    }
+
+    public override void Enter()
+    {
+        base.Enter();
+
+        _Hero.Animator.SetBool(_Hero.LastAnimBoolName, true);
+        _Hero.Animator.speed = 0;
+        _Hero.CapsuleCollider.enabled = false;
+        _Hero.transform.position = new Vector3(_Hero.transform.position.x, _Hero.transform.position.y, 10);
+
+        StateTimer = .15f;
+    }
+
+    public override void Update()
+    {
+        base.Update();
+
+        if (StateTimer > 0)
+        {
+            Rb.linearVelocity = new Vector2(0, 10);
+        }
+    }
+
+    public override void Exit()
+    {
+        base.Exit();
+    }
+}
---
+++ First-RPG-Game/Assets/Scripts/Enemies/SuperHero/HeroIdleState.cs
@@ -0,0 +1,36 @@
+﻿using Enemies;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+public class HeroIdleState : HeroGroundedState
+{
+    public HeroIdleState(Enemy enemyBase, EnemyStateMachine stateMachine, string animBoolName, Hero Hero) : base(enemyBase, stateMachine, animBoolName, Hero)
+    {
+    }
+
+    public override void Enter()
+    {
+        base.Enter();
+
+        StateTimer = Hero.idleTime;
+    }
+
+    public override void Update()
+    {
+        base.Update();
+
+        //if (StateTimer <= 0)
+        //{
+        //    StateMachine.ChangeState(Hero.MoveState);
+        //}
+    }
+
+    public override void Exit()
+    {
+        base.Exit();
+    }
+}
