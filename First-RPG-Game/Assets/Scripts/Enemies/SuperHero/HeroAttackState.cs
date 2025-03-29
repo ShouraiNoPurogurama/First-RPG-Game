9d2f457a1aaa74a453b18960270e25f51d415f60
Datdater <datnqse173081@fpt.edu.vn>
2025-03-29 13:15:39 +0700

    Fix general

---
+++ First-RPG-Game/Assets/Scripts/Enemies/SuperHero/HeroAttackState.cs
@@ -0,0 +1,41 @@
+﻿using Enemies;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+public class HeroAttackState : EnemyState
+{
+    private Hero _Hero;
+    public HeroAttackState(Enemy enemyBase, EnemyStateMachine stateMachine, string animBoolName, Hero Hero) : base(enemyBase, stateMachine, animBoolName)
+    {
+        _Hero = Hero;
+    }
+
+    public override void Enter()
+    {
+        base.Enter();
+    }
+
+    public override void Update()
+    {
+        base.Update();
+
+        _Hero.SetZeroVelocity();
+
+        if (TriggerCalled)
+        {
+            _Hero.ShootArrows();
+            TriggerCalled = false;
+            _Hero.lastTimeAttacked = Time.time;
+            StateMachine.ChangeState(_Hero.BattleState);
+        }
+    }
+
+    public override void Exit()
+    {
+        base.Exit();
+    }
+}
